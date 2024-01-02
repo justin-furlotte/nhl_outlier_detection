1f9d88134540a845d091f2f61d7e235b83476b96
Justin Furlotte <justin.furlotte@fiddlehead.com>
2024-01-01 21:24:48 -0400

    Working models, which are now tuned and pickled

--- src/main.py
+++ src/main.py
@@ -4,42 +4,13 @@ from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.linear_model import LogisticRegression, Ridge, Lasso
 from sklearn.svm import SVC, SVR
 from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, StandardScaler
-
 from sklearn.pipeline import Pipeline, make_pipeline
-
+from sklearn.model_selection import GridSearchCV
+from utils.hyperparameter_tuning import Tuner
 import pandas as pd
 import numpy as np
+import pickle
 pd.set_option('expand_frame_repr', False)  # Display full dataframes while printing them
-
-
-def create_df(dfs):
-    for key in dfs.keys():
-        # tonjt = self.dfs[key].columns
-        dfs[key] = dfs[key].loc[dfs[key]["situation"] == "all"].drop(columns="situation")
-    return dfs
-def create_x_y_train(self, season_start, season_end):
-
-    X_all_years = [self.dfs[key].drop(columns="I_F_goals") for key in self.dfs.keys()]
-    y_all_years = [self.dfs[key]["I_F_goals"] for key in self.dfs.keys()]
-
-    start = int(season_start[:2])
-    finish = int(season_end[:2])
-
-    X = []
-    y = []
-
-    for i in np.arange(start, finish + 1):
-        dfX = X_all_years[i - start]
-
-        playerid_new = {playerid: str(playerid) + "_" + str(i) + "_" + str(i + 1) for playerid in list(dfX.index)}
-        dfX = dfX.rename(index=playerid_new)
-        X.append(dfX)
-
-        dfy = y_all_years[i - start]
-        dfy = dfy.rename(index=playerid_new)
-        y.append(dfy)
-
-    return pd.concat(X), pd.concat(y)
 
 
 if __name__ == "__main__":
@@ -68,16 +39,80 @@ if __name__ == "__main__":
         id_name_map[player_id] = name
 
     train_seasons = [train_start + i for i in range(train_end - train_start + 1)]
-    df_train = df.loc[df.index.get_level_values('season').isin(train_seasons)]
+    df_train = df.loc[df.index.get_level_values("season").isin(train_seasons)]
+    df_test = df.loc[df.index.get_level_values("season") == train_end + 1]
 
     X_train = df_train.drop(target, axis=1)
     y_train = df_train[target]
+    X_test = df_test.drop(target, axis=1)
+    y_test = df_test[target]
 
     cleaner = Preprocessor()
     preprocessor = cleaner.create_preprocessor(X_train=X_train)
     new_feature_names = cleaner.get_new_feature_names(X=X_train)
 
-    pipe_lasso = make_pipeline(preprocessor, StandardScaler(), Lasso(max_iter=1000))
+    tuner = Tuner(preprocessor=preprocessor, X_train=X_train, y_train=y_train)
+
+    # ---------------- Create a pipeline for some models ----------------
+
+    # Already trained a bunch of models and pickled them; let's load them
+    with open(f"../models/LASSO_pickle_{train_start}_{train_end}", "rb") as f:
+        tuned_lasso_pipe = pickle.load(f)
+    with open(f"../models/RFR_pickle_{train_start}_{train_end}", "rb") as f:
+        tuned_rfr_pipe = pickle.load(f)
+    with open(f"../models/SVR_pickle_{train_start}_{train_end}", "rb") as f:
+        svr_pipe = pickle.load(f)
+    with open(f"../models/SR_pickle_{train_start}_{train_end}", "rb") as f:
+        ensemble = pickle.load(f)
+
+    # # LASSO Regression
+    # lasso_pipe = make_pipeline(preprocessor, StandardScaler(), Lasso(max_iter=1000))
+    # tuned_lasso_pipe = tuner.tune_lasso(lasso_pipe=lasso_pipe, verbose=True)
+    #
+    # # Random forest regression
+    # rfr_pipe = make_pipeline(preprocessor, RandomForestRegressor())
+    # tuned_rfr_pipe = tuner.tune_random_forest_regressor(rfr_pipe=rfr_pipe, verbose=True)
+    #
+    # # SVM Regression
+    # # Unfortunately it is too slow to do hyperparameter optimization on SVR, but it is still giving
+    # # decent results with a regularization strength of 1 and Gaussian RBF kernel
+    # svr_pipe = make_pipeline(preprocessor, StandardScaler(), SVR(kernel="rbf", C=1.0))
+    #
+    # # Create and tune a stacking ensemble model with Ridge as the final regressor.
+    # ensemble = tuner.tune_ensemble(pipe_lasso=tuned_lasso_pipe, pipe_rfr=tuned_rfr_pipe, pipe_svr=svr_pipe, verbose=True)
+    #
+    # # save models
+    # print("Saving models...\n")
+    # with open(f"../models/LASSO_pickle_{train_start}_{train_end}", "wb") as f:
+    #     pickle.dump(tuned_lasso_pipe, f)
+    # with open(f"../models/RFR_pickle_{train_start}_{train_end}", "wb") as f:
+    #     pickle.dump(tuned_rfr_pipe, f)
+    # with open(f"../models/SVR_pickle_{train_start}_{train_end}", "wb") as f:
+    #     pickle.dump(svr_pipe, f)
+    # with open(f"../models/SR_pickle_{train_start}_{train_end}", "wb") as f:
+    #     pickle.dump(ensemble, f)
+
+    ensemble_coefs = ensemble.final_estimator_.coef_
+    print(f"Stacking estimator (Ridge) coefficients:")
+    print(f"LASSO: {ensemble_coefs[0]}")
+    print(f"RFR: {ensemble_coefs[1]}")
+    print(f"SVR: {ensemble_coefs[2]}")
+
+    # Try it out
+    tuned_lasso_pipe.fit(X_train, y_train)
+    tuned_rfr_pipe.fit(X_train, y_train)
+    svr_pipe.fit(X_train, y_train)
+    ensemble.fit(X_train, y_train)
+
+    compare = pd.DataFrame(y_test.copy())
+    compare["LASSO"] = tuned_lasso_pipe.predict(X_test)
+    compare["RFR"] = tuned_rfr_pipe.predict(X_test)
+    compare["SVR"] = svr_pipe.predict(X_test)
+    compare["Ensemble"] = ensemble.predict(X_test)
+    compare = compare.apply(round)
+
+    year = 2019
+    compare.loc[compare.index.get_level_values("season") == year]
 
     print("DONE")
 
---
+++ utils/hyperparameter_tuning.py
@@ -0,0 +1,89 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import Ridge, Lasso
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import (
+    GridSearchCV,
+    RandomizedSearchCV,
+    cross_validate,
+)
+from sklearn.ensemble import StackingRegressor
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.compose._column_transformer import ColumnTransformer
+from sklearn.pipeline import Pipeline
+
+
+class Tuner:
+    def __init__(self, preprocessor: ColumnTransformer, X_train: pd.DataFrame, y_train: pd.DataFrame):
+        self.preprocessor = preprocessor
+        self.X_train = X_train
+        self.y_train = y_train
+
+    def tune_random_forest_regressor(self, rfr_pipe: Pipeline, d: int = None, verbose: bool = False) -> Pipeline:
+
+        if d is None:
+            d = int(self.X_train.shape[1])
+
+        parameters_rfr = {
+            "randomforestregressor__max_depth": np.arange(int(np.floor(np.sqrt(d)/2)), int(np.floor(np.sqrt(d)*2))),
+            "randomforestregressor__n_estimators": np.arange(20, 100)
+        }
+        rs_rfr = RandomizedSearchCV(rfr_pipe, parameters_rfr, scoring="neg_mean_absolute_error")
+        rs_rfr.fit(self.X_train, np.ravel(self.y_train))
+
+        max_depth_best = rs_rfr.best_params_["randomforestregressor__max_depth"]
+        n_estimators_best = rs_rfr.best_params_["randomforestregressor__n_estimators"]
+
+        if verbose:
+            print(f"RFR Best maximum depth: {max_depth_best}")
+            print(f"RFR Best number of estimators: {n_estimators_best}")
+
+        tuned_rfr_pipe = make_pipeline(
+            self.preprocessor,
+            StandardScaler(),
+            RandomForestRegressor(max_depth=max_depth_best, n_estimators=n_estimators_best)
+        )
+
+        return tuned_rfr_pipe
+
+    def tune_lasso(
+            self,
+            lasso_pipe: Pipeline,
+            verbose: bool = False
+    ) -> Pipeline:
+
+        parameters_lasso = {"lasso__alpha": np.linspace(0.1, 5, 10)}  # Some values for the regularization strength
+        gs_lasso = GridSearchCV(lasso_pipe, parameters_lasso, scoring="neg_mean_absolute_error")
+
+        # Since LASSO is fast, we can perform a grid search for the optimal alpha
+        gs_lasso.fit(self.X_train, self.y_train)
+        alpha_best_lasso = gs_lasso.best_params_["lasso__alpha"]
+        if verbose:
+            print("\nGrid Search Results:\n")
+            print(f"Optimal alpha: {alpha_best_lasso}")
+            print(pd.DataFrame(gs_lasso.cv_results_).head(5))
+
+        # Update the LASSO pipeline with the optimal alpha
+        lasso_pipe = make_pipeline(self.preprocessor, StandardScaler(), Lasso(alpha=alpha_best_lasso))
+
+        return lasso_pipe
+
+    def tune_ensemble(self, pipe_lasso: Pipeline, pipe_rfr: Pipeline, pipe_svr: Pipeline, verbose: bool = False):
+
+        # Ensemble Model (Stacking)
+        sr_ridge = StackingRegressor(
+            estimators=[
+                ("lasso", pipe_lasso),
+                ("rfr", pipe_rfr),
+                ("svr", pipe_svr)
+            ],
+            final_estimator=Ridge(alpha=1)
+        )
+
+        if verbose:
+            print("Mean of cross validation results - stacking model:")
+            print(np.mean(pd.DataFrame(
+                cross_validate(sr_ridge, self.X_train, np.ravel(self.y_train), scoring="neg_mean_absolute_error"))))
+
+        return sr_ridge
